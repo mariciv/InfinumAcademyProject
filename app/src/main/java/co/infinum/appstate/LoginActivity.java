e8372c16ca97681950763443d44075f4a7705141
Ivan Maric <ivan.maric@infinum.hr>
2015-07-03 09:47:13 +0200

    Setup initial layouts and activities

--- app/src/main/java/co/infinum/appstate/LoginActivity.java
+++ app/src/main/java/co/infinum/appstate/LoginActivity.java
@@ -1,14 +1,90 @@
 package co.infinum.appstate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextWatcher textChange = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (!passwordEditText.getText().toString().isEmpty() && !usernameEditText.getText().toString().isEmpty()) {
+                loginButton.setEnabled(true);
+            } else {
+                loginButton.setEnabled(false);
+            }
+        }
+    };
+
+    private View welcome;
+
+    private View loginLayout;
+
+    private View welcomeLayout;
+
+    private EditText passwordEditText;
+
+    private EditText usernameEditText;
+
+    private Button loginButton;
+
+    private CheckBox remeberMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Inject views
+        passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+        usernameEditText = (EditText) findViewById(R.id.username_edit_text);
+        loginButton = (Button) findViewById(R.id.login_button);
+        welcome = findViewById(R.id.welcome);
+        loginLayout = findViewById(R.id.login_layout);
+        welcomeLayout = findViewById(R.id.welcome_layout);
+        remeberMe = (CheckBox) findViewById(R.id.remember_check_box);
+
+        // Setup text watchers
+        passwordEditText.addTextChangedListener(textChange);
+        usernameEditText.addTextChangedListener(textChange);
+
+        // Set click listeners
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginLayout.setVisibility(View.VISIBLE);
+                welcomeLayout.setVisibility(View.GONE);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("USERNAME", usernameEditText.getText().toString());
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 
 
---
+++ app/src/main/java/co/infinum/appstate/MainActivity.java
@@ -0,0 +1,30 @@
+package co.infinum.appstate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Ivan on 02/07/15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private TextView usernameTextView;
+
+    private String username = "";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        //Inject views
+        usernameTextView = (TextView) findViewById(R.id.username_text);
+
+        if (getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().containsKey("USERNAME")) {
+            username = getIntent().getStringExtra("USERNAME");
+        }
+
+        usernameTextView.setText(username);
+    }
+}
